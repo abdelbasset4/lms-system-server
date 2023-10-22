e5eb819dbd0acfa3c855bb37196a93460acfdec2
abdelbasset4 <benatmani1999@gmail.com>
2023-10-22 21:27:05 +0100

    get all and single course

--- routes/courseRoute.ts
+++ routes/courseRoute.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 const courseRoute = express.Router()
 import { isAllowed, isAuthenticated } from '../middleware/auth';
-import { editCourse, uploadCourse } from '../services/courseService';
+import { editCourse, getAllCourses, getSingleCourse, uploadCourse } from '../services/courseService';
 
 courseRoute.post('/add-course', isAuthenticated, isAllowed('admin'), uploadCourse)
 courseRoute.put('/edit-course/:id', isAuthenticated, isAllowed('admin'), editCourse)
+courseRoute.get('/get-courses/:id', getSingleCourse)
+courseRoute.get('/get-courses', getAllCourses)
 
 export default courseRoute;
--- services/courseService.ts
+++ services/courseService.ts
@@ -4,6 +4,7 @@ import cloudinary from "cloudinary";
 import { addCourse } from "../features/course.features";
 import ApiError from "../utils/ApiError";
 import Course from "../models/Course";
+import { redis } from "../config/redis";
 
 // @desc    Add course
 // @route   POST /api/v1/course/add-course
@@ -69,3 +70,68 @@ export const editCourse = asyncHandler(
     }
   }
 );
+
+// @desc Get Single Course without purchase
+// @route GET /api/v1/course/get-courses/:id
+// @access Public
+export const getSingleCourse = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const courseId= req.params.id;
+      const isCashCourse = await redis.get(courseId);
+      if(isCashCourse) {
+        const course = JSON.parse(isCashCourse)
+        res.status(200).json({
+          success: true,
+          course,
+        });
+      }else{
+        const course = await Course.findById(req.params.id).select("-courseData.questions -courseData.links -courseData.suggestions -courseData.videoUrl");
+        if (!course) {
+          return next(new ApiError("Course not found", 404));
+        }
+        await redis.set(courseId, JSON.stringify(course))
+        res.status(200).json({
+          success: true,
+          course,
+        });
+      }
+      
+    } catch (error: any) {
+      return next(new ApiError(error.message, 400));
+    }
+  }
+);
+
+// @desc Get All Courses without purchase
+// @route GET /api/v1/course/get-courses/
+// @access Public
+export const getAllCourses = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const courseId= req.params.id;
+      const isCashCourse = await redis.get("allCourses");
+      if(isCashCourse){
+        const course = JSON.parse(isCashCourse)
+        res.status(200).json({
+          success: true,
+          course,
+        });
+      }else{
+        const course = await Course.find().select("-courseData.questions -courseData.links -courseData.suggestions -courseData.videoUrl");
+        if (!course) {
+          return next(new ApiError("Course not found", 404));
+        }
+        await redis.set("allCourses", JSON.stringify(course))
+        res.status(200).json({
+          success: true,
+          course,
+        });
+      }
+      
+    } catch (error: any) {
+      return next(new ApiError(error.message, 400));
+    }
+  }
+);
+
